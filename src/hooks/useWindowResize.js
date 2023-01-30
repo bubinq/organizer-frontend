f8d4ca4ce9db742bb76fd16ae92284ed74ce34d9
bubinq <emilbumov@gmail.com>
2023-01-30 21:19:57 +0200

    Latest fix

--- src/hooks/useWindowResize.js
+++ src/hooks/useWindowResize.js
@@ -4,6 +4,7 @@ export const useWindowResize = () => {
   const [size, setSize] = useState({ width: undefined });
 
   useEffect(() => {
+    console.log(size);
     const resizeHandler = () => {
       setSize({ width: window.innerWidth });
     };
@@ -12,6 +13,7 @@ export const useWindowResize = () => {
     return () => {
       window.removeEventListener("resize", resizeHandler);
     };
+    //eslint-disable-next-line
   }, []);
 
   return size;
