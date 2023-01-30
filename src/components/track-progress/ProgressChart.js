51351e7a99113ee607d26367bef1388cb3924cdf
bubinq <emilbumov@gmail.com>
2023-01-30 14:51:39 +0200

    Changed Display Progress UI

--- src/components/track-progress/ProgressChart.js
+++ src/components/track-progress/ProgressChart.js
@@ -5,14 +5,13 @@ import styles from "./ProgressPage.module.css";
 Chart.register(ArcElement);
 
 export const ProgressChart = ({ day }) => {
-    console.log(day);
   const data = {
     labels: ["Complete", "Incomplete"],
     datasets: [
       {
         label: "Display Percentage",
         data: [day.progress, 100 - day.progress],
-        backgroundColor: ["#4AF193", "#000"],
+        backgroundColor: ["#4AF193", "#fff"],
         hoverOffset: 3,
         borderRadius: 5,
         spacing: 5,
@@ -24,8 +23,8 @@ export const ProgressChart = ({ day }) => {
     type: "doughnut",
     data: data,
     options: {
-      cutout: 65,
-      radius: 50,
+      cutout: 5,
+      radius: 10,
     },
   };
   return (
